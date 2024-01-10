86ded3d1ad91766f3024047b2e2d1925ac71f4e1
kudr.max <mdkudryashov@edu.hse.ru>
2024-01-10 20:39:19 +0300

    Добавлен конструктор перемещения

--- source/MyVector.h
+++ source/MyVector.h
@@ -6,35 +6,42 @@ class MyVector {
 public:
     // конструкторы
     MyVector() = default;
-    MyVector(size_t size) : m_size(size), arr(new int[size]) {};
+    MyVector(size_t size) : m_size(size), m_vec(new int[size]) {};
     MyVector(size_t size, int num) : MyVector(size) {
         for (size_t i = 0; i < m_size; i++)
-            arr[i] = num;
+            m_vec[i] = num;
     };
     MyVector(std::initializer_list<int> list) : MyVector(list.size()) {
         for (auto it = list.begin(); it != list.end(); it++)
-            arr[std::distance(list.begin(), it)] = *it;
+            m_vec[std::distance(list.begin(), it)] = *it;
     }
 
     // деструктор
     ~MyVector() {
-        delete[] arr;
-        arr = nullptr;
+        delete[] m_vec;
+        m_vec = nullptr;
     }
 
     // конструктор копирования
-    MyVector(const MyVector& other_arr) : MyVector(other_arr.m_size) {
-        for (int i = 0; i < other_arr.m_size; i++)
-            arr[i] = *(other_arr.arr + i);
+    MyVector(const MyVector& other_vec) : MyVector(other_vec.m_size) {
+        for (int i = 0; i < other_vec.m_size; i++)
+            m_vec[i] = *(other_vec.m_vec + i);
+    }
+
+    // конструктор перемещения
+    MyVector(MyVector&& other_vec) {
+        std::swap(other_vec.m_size, m_size);
+        std::swap(other_vec.m_vec, m_vec);
     }
 public:
 //    size_t size() { return m_size; }
     void print() {
+        std::cout << "vec: ";
         for (size_t i = 0; i < m_size; i++)
-            std::cout << arr[i];
+            std::cout << m_vec[i];
         std::cout << std::endl;
     }
 private:
-    int* arr;
+    int* m_vec;
     size_t m_size;
 };
--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,10 @@
 
 int main() {
     MyVector vec1 = { 1, 2, 3, 4, 5 };
-    MyVector vec2 = vec1;
-    MyVector vec3(vec1);
+    MyVector vec2(std::move(vec1));
+
 
     vec1.print();
     vec2.print();
-    vec3.print();
     return 0;
 }
