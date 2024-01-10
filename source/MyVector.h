5dcda55e01e03a86528f5a658edf0c0404e4d402
kudr.max <mdkudryashov@edu.hse.ru>
2024-01-10 21:12:40 +0300

    Добавлен оператор +=

--- source/MyVector.h
+++ source/MyVector.h
@@ -8,78 +8,90 @@ public:
     explicit MyVector(size_t size); // конструктор от одного аргумента
     MyVector(size_t size, int num); // конструктор от двух аргументов
     MyVector(std::initializer_list<int> list); // конструктор от std::initializer_list
-    MyVector(const MyVector& other_vec); // конструктор копирования
-    MyVector(MyVector&& other_vec) noexcept; // конструктор перемещения
+    MyVector(const MyVector& obj); // конструктор копирования
+    MyVector(MyVector&& obj) noexcept; // конструктор перемещения
     ~MyVector(); // деструктор
 public:
-    MyVector& operator=(const MyVector& vec); // оператор копирующего присваивания
-    MyVector& operator=(MyVector&& vec) noexcept; // оператор move присваивания
-    MyVector& operator+=(const MyVector& vec);
+    MyVector& operator=(const MyVector& obj); // оператор копирующего присваивания
+    MyVector& operator=(MyVector&& obj) noexcept; // оператор move присваивания
+    MyVector& operator+=(const MyVector& obj); // оператор +=
     MyVector& operator-();
     MyVector& operator++();
     const MyVector operator++(int);
 public:
-    bool operator==(const MyVector& vec) const noexcept;
-    bool operator!=(const MyVector& vec) const noexcept;
+    bool operator==(const MyVector& obj) const noexcept;
+    bool operator!=(const MyVector& obj) const noexcept;
 public:
     MyVector& operator[](size_t index);
     const MyVector& operator[](size_t index) const;
 public:
 //    size_t size() { return m_size; }
     void print() {
-        std::cout << "vec: ";
+        std::cout << "obj: ";
         for (size_t i = 0; i < m_size; i++)
-            std::cout << m_vec[i];
+            std::cout << m_arr[i];
         std::cout << std::endl;
     }
 private:
-    int* m_vec;
+    int* m_arr;
     size_t m_size;
 };
 
-MyVector::MyVector(size_t size) : m_size(size), m_vec(new int[size]) {};
+MyVector::MyVector(size_t size) : m_size(size), m_arr(new int[size]) {};
 
 MyVector::MyVector(size_t size, int num) : MyVector(size) {
     for (size_t i = 0; i < m_size; i++)
-        m_vec[i] = num;
+        m_arr[i] = num;
 }
 
 MyVector::MyVector(std::initializer_list<int> list) : MyVector(list.size()) {
     for (auto it = list.begin(); it != list.end(); it++)
-        m_vec[std::distance(list.begin(), it)] = *it;
+        m_arr[std::distance(list.begin(), it)] = *it;
 }
 
 MyVector::~MyVector() {
-    delete[] m_vec;
-    m_vec = nullptr;
+    delete[] m_arr;
+    m_arr = nullptr;
 }
 
-MyVector::MyVector(const MyVector& other_vec) : MyVector(other_vec.m_size) {
-    for (int i = 0; i < other_vec.m_size; i++)
-        m_vec[i] = *(other_vec.m_vec + i);
+MyVector::MyVector(const MyVector& obj) : MyVector(obj.m_size) {
+    for (int i = 0; i < obj.m_size; i++)
+        m_arr[i] = *(obj.m_arr + i);
 }
 
-MyVector::MyVector(MyVector&& other_vec) noexcept {
-    std::swap(other_vec.m_size, m_size);
-    std::swap(other_vec.m_vec, m_vec);
+MyVector::MyVector(MyVector&& obj) noexcept {
+    std::swap(obj.m_size, m_size);
+    std::swap(obj.m_arr, m_arr);
 }
 
-MyVector& MyVector::operator=(const MyVector& vec) {
-    if (&vec == this)
+MyVector& MyVector::operator=(const MyVector& obj) {
+    if (&obj == this)
         return *this;
-    delete[] m_vec;
-    m_size = vec.m_size;
-    m_vec = new int[m_size];
-    for (int i = 0; i < m_size; i++)
-        m_vec[i] = vec.m_vec[i];
+    delete[] m_arr;
+    m_size = obj.m_size;
+    m_arr = new int[m_size];
+    for (size_t i = 0; i < m_size; i++)
+        m_arr[i] = obj.m_arr[i];
     return *this;
 }
 
-MyVector& MyVector::operator=(MyVector&& vec) noexcept {
-    if (&vec == this)
+MyVector& MyVector::operator=(MyVector&& obj) noexcept {
+    if (&obj == this)
         return *this;
-    delete[] m_vec;
-    std::swap(vec.m_size, m_size);
-    std::swap(vec.m_vec, m_vec);
+    delete[] m_arr;
+    std::swap(obj.m_size, m_size);
+    std::swap(obj.m_arr, m_arr);
+    return *this;
+}
+
+MyVector& MyVector::operator+=(const MyVector& obj) {
+    auto temp = new MyVector(m_size + obj.m_size);
+    for (size_t i = 0; i < m_size; i++)
+        temp->m_arr[i] = m_arr[i];
+    for (size_t i = m_size; i < m_size + obj.m_size; i++)
+        temp->m_arr[i] = obj.m_arr[i - m_size];
+    delete[] m_arr;
+    m_arr = temp->m_arr;
+    m_size = temp->m_size;
     return *this;
 }
--- source/main.cpp
+++ source/main.cpp
@@ -2,12 +2,12 @@
 
 int main() {
     MyVector vec1 = { 1, 2, 3, 4, 5 };
-    MyVector vec2;
-    vec2 = vec1;
+    MyVector vec2 = { 0, 0 };
+    vec1 += vec2;
 
 
+//    vec1.print();
     vec1.print();
-    vec2.print();
 
     return 0;
 }
